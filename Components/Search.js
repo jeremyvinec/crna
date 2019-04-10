21b2380f90cb965db1d162a28c2990aec18a5649
jeremyvinec <jeremy.yvinec@gmail.com>
2019-04-10 14:21:35 +0200

    poster film

--- Components/Search.js
+++ Components/Search.js
@@ -1,3 +1,5 @@
+// Components/Search.js
+
 import React from 'react'
 import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
 import FilmItem from './FilmItem'
@@ -5,60 +7,59 @@ import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
 
-    constructor(props){
-        super(props)
-        this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
-        this.state = {
-          films: []
+  constructor(props) {
+    super(props)
+    this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+    this.state = {
+      films: []
     }
+  }
 
-    _searchTextInputChanged(text){
-        this.searchedText = text
+  _loadFilms() {
+    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+          this.setState({ films: data.results })
+      })
     }
+  }
 
-    _loadFilms() {
-        //console.log(this.state.searchedText) // un log pour vérifier qu'on a bien le text du textinput
-        if(this.state.searchedText.length > 0){ // seulment si le text recherché n'est pas valide
-            getFilmsFromApiWithSearchedText(this.state.searchedText)
-            .then(data => {
-                this.setState({ films: data.results })
-            });
-        }
-    }
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
+  }
 
-    render(){
-        return(
-            // Ici on rend à l'écran les éléments graphiques de notre component custom Search
-            <View style={styles.main_container}>
-                <TextInput
-                    style= {styles.textinput} 
-                    placeholder='Titre du film'
-                    onChangeText={(text) => this._searchTextInputChanged(text)}
-                />
-                <Button title='Recherche' onPress={() => this._loadFilms()}/>
-                <FlatList
-                    data={this.state.films}
-                    keyExtractor={(item) => item.id.toString()}
-                    renderItem={({item}) => <FilmItem film={item}/>}
-                />
-            </View>
-        )
-    }
+  render() {
+    console.log("RENDER")
+    return (
+      <View style={styles.main_container}>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
+        <FlatList
+          data={this.state.films}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({item}) => <FilmItem film={item}/>}
+        />
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    main_container{
-        flex: 1,
-        marginTop : 20
-    },
-    textinput: {
-        marginLeft: 5, 
-        marginRight: 5, 
-        height: 50,       
-        borderColor: '#000000', 
-        borderWidth: 1, 
-        paddingLeft: 5
-    }
+  main_container: {
+    flex: 1,
+    marginTop: 20
+  },
+  textinput: {
+    marginLeft: 5,
+    marginRight: 5,
+    height: 50,
+    borderColor: '#000000',
+    borderWidth: 1,
+    paddingLeft: 5
+  }
 })
 
 export default Search
